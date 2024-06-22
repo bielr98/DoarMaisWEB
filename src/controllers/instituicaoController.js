dd2771aa6b32c7bed2ef6357d6f2f418edc13bd5
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-06-22 02:06:28 -0300

    login e logout funcionando

--- src/controllers/instituicaoController.js
+++ src/controllers/instituicaoController.js
@@ -43,8 +43,33 @@ function cadastrarUsuario(req, res) {
     });
 }
 
+function loginView(req, res) {
+    res.render("index.html");
+}
+
+function loginUsuario(req, res) {
+    const {email, senha } = req.body;
+    UsuarioModel.findOne({ where: { email, senha } })
+    .then(usuario => {
+        if (usuario) {
+            //consulta o banco e pega o primeiro Nome
+            const primeiroNome = usuario.nome.split(' ')[0];
+            //pagina que ele sera redirecionado ao logar e passar o primeiro nome dele
+            res.render('home.html', { nome: primeiroNome }); 
+        } else {
+            res.redirect('/?login=false');
+        }
+    })
+    .catch(err => {
+        console.error(err);
+        res.redirect('/?login=false');
+    });
+}
+
 module.exports = {
     indexView,
     criarContaView,
-    cadastrarUsuario
+    cadastrarUsuario,
+    loginView,
+    loginUsuario
 }
--- src/routes/instituicaoRoutes.js
+++ src/routes/instituicaoRoutes.js
@@ -6,5 +6,15 @@ const instituicaoController = require('../controllers/instituicaoController')
 router.get('/', instituicaoController.indexView);
 router.get('/criar_conta', instituicaoController.criarContaView);
 router.post('/cadastrar_usuario', instituicaoController.cadastrarUsuario);
+router.post('/acessar', instituicaoController.loginUsuario);
+
+router.get('/home', (req, res) => {
+    res.render('home.html');// Rota para servir a página home.html
+})
+
+router.get('/logout', (req, res) => {
+    // Aqui você pode destruir a sessão se estiver usando sessões
+    res.redirect('/');
+});
 
 module.exports = router;
